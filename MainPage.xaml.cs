b4f1a830c94a1e5799ce0685700d8d53f71a4b10
omar alshobky <93231975za@gmail.com>
2020-10-14 12:53:02 +0200

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwasemApp9
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+      
+
+        public void Click_me(Object sender,EventArgs e)
+        {
+            om.Text ="welcome mr "+ etext.Text;
+            om.TextColor = Color.Purple;
+        }
+
+        int count = 0;
+
+        private void Click_Button(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
---
+++ TabbedDemo.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwasemApp9
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TabbedDemo : TabbedPage
+    {
+
+        public TabbedDemo()
+        {
+            InitializeComponent();
+            images.Source = ImageSource.FromUri(new Uri("https://pbs.twimg.com/profile_images/1229464423770787840/yLXLXE-0_400x400.jpg"));
+        }
+
+    }
+}
